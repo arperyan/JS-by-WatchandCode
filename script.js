56b83104c734d2be1fb205360400516765daa804
DESKTOP-C8T97K1\Ryan_ <ryan_arpe@hotmail.com>
2017-09-28 15:22:07 +0100

    Add For loops and if statement

--- script.js
+++ script.js
@@ -2,7 +2,20 @@ var todosList = {
   //todos: ['item 1','item 2','item 3'],
   todos: [],
   todosDisplay: function(){
-    console.log('To dos: ',this.todos);
+    if(this.todos.length === 0){
+        console.log('Todos list is empty');
+    } else {
+        console.log('To dos: ');
+        for (var i =0; i < this.todos.length;i++) {
+          if (this.todos[i].completed === true) {
+            console.log('(x)',this.todos[i].todosText);
+          } else {
+            console.log('()',this.todos[i].todosText);
+          }
+
+
+      }
+    }
   },
   todosAdd: function(todosText){
     this.todos.push({
@@ -23,6 +36,27 @@ var todosList = {
   toggleComplete: function(pos){
     var todo = this.todos[pos];
     todo.completed = !todo.completed;
+    this.todosDisplay();
+  },
+  toggleAll: function () {
+    var totalTodos = this.todos.length;
+    var completedTodos = 0;
+
+    for (var i = 0; i < totalTodos; i++) {
+      if (this.todos[i].completed === true) {
+        completedTodos++;
+      }
+    }
+
+    if (completedTodos === totalTodos) {
+      for (var i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = false;
+      }
+    } else {
+      for (var i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = true;
+      }
+    }
     this.todosDisplay();
   }
 }
